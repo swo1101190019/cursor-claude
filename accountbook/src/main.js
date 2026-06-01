f72ac39d2555334b061d74dd1830dddbd421f3bd
chenxing <122242762+swo1101190019@users.noreply.github.com>
2026-06-01 14:39:32 +0800

    Delete accountbook directory

--- accountbook/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import router from './router/index.js'
-import App from './App.vue'
-import './styles/global.css'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
